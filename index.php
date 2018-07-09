fd74034cea0874514e1ad131b1939fe20924f383
rahul shahare <rahulshahare@gmail.com>
2018-07-09 13:14:01 +0530

    setup the page , adding css for extra effect

--- index.php
+++ index.php
@@ -8,37 +8,36 @@
 
     <!-- Bootstrap -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
+    <style type="text/css">
+    body{
+        background-color: #337ab724;
+    }
+        .form-control , .btn{
+            border-radius:50px;
+        }
+    </style>
   </head>
   <body>
     <div class="container">
         <div class="row">
-            <div class="col-md-6 col-md-offset-3">
-                <div class="row">
-                    <div class="col-md-10 col-md-offset-1 text-center">
-                        <h1>Login to Twitter</h1>
+            <div class="col-md-6 col-md-offset-3 text-center">
+               
+                        <h1>Login to Twitter</h1><br/><br/><br/>
                         <form>
                         <div class="form-group">
-                            <label for="exampleInputEmail1">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                            
+                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter Your Email">
                         </div>
                         <div class="form-group">
-                            <label for="exampleInputPassword1">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            
+                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Enter Your Password">
                         </div>
-                        <div class="form-group">
-                            <label for="exampleInputFile">File input</label>
-                            <input type="file" id="exampleInputFile">
-                            <p class="help-block">Example block-level help text here.</p>
-                        </div>
-                        <div class="checkbox">
-                            <label>
-                            <input type="checkbox"> Check me out
-                            </label>
-                        </div>
-                        <button type="submit" class="btn btn-default">Submit</button>
+                       
+                        <button type="submit" class="btn btn-primary btn-block">Login</button>
                         </form>
-                    </div>
-                </div>
+                        <br/><br/>
+                        <p><a href="#">Forgot Password?</a> | <a href="#">SignUp for twitter</a> </p>
+                   
             </div>
         </div>
     </div>
